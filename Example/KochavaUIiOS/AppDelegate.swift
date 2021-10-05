8fbdb8715a83156cabac9fc1eab5cf780c80b3ff
John Bushnell <jbushnell@kochava.com>
2021-10-05 14:48:02 -0700

    5.0.0

--- Example/KochavaUIiOS/AppDelegate.swift
+++ Example/KochavaUIiOS/AppDelegate.swift
@@ -28,6 +28,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate
         // ⓘ Set to trace.  This will result in the KVACoreProduct being registered, and this will be prefaced by any other known modules which are included.  This will be printed to the log where they can be inspected.
         KVALog.shared.level = KVALogLevel.trace
 
+        // KVAUIProduct
+        KVAUIProduct.shared.register()
+        
         return true
     }
 }
